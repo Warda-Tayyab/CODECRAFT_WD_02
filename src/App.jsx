9f683b634147ad13168e614b2baeb14e51e8367e
Warda Tayyab <wardat057@gmail.com>
2024-12-08 18:27:19 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Watch from './Watch'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <Watch></Watch></>
+  )
+}
+
+export default App
